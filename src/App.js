b309de28c1d139da78050f946d029a399f973ea4
vinicius ribeiro <viniciusrad@yahoo.com.br>
2024-10-14 23:07:22 -0300

    wip - teste reconhecimento

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import * as faceapi from 'face-api.js';
 import './App.css';
 
 function App() {
+  const [gender, setGender] = useState(null);
+
+  useEffect(() => {
+    loadModels();
+  }, []);
+
+  const loadModels = async () => {
+    await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+    await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+    await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
+    await faceapi.nets.ageGenderNet.loadFromUri('/models');
+  };
+
+  const handleImageUpload = async (e) => {W
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = async (event) => {
+        const img = document.createElement('img');
+        img.src = event.target.result;
+        img.style.maxWidth = '100%';
+        img.style.marginTop = '20px';
+        
+        const container = document.querySelector('.App-header');
+        container.appendChild(img);
+
+        // Realizar detecção facial e classificação de gênero
+        const detections = await faceapi.detectSingleFace(img, new faceapi.TinyFaceDetectorOptions())
+          .withFaceLandmarks()
+          .withAgeAndGender();
+
+        if (detections) {
+          setGender(detections.gender);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Creche App</h1>
+        <h1>Creche App</h1>
 
         {window.innerWidth <= 768 && (
           <div>
@@ -13,22 +53,10 @@ function App() {
               type="file"
               accept="image/*"
               capture="camera"
-              onChange={(e) => {
-                const file = e.target.files[0];
-                if (file) {
-                  const reader = new FileReader();
-                  reader.onload = (event) => {
-                    const img = document.createElement('img');
-                    img.src = event.target.result;
-                    img.style.maxWidth = '100%';
-                    img.style.marginTop = '20px';
-                    document.querySelector('.App-header').appendChild(img);
-                  };
-                  reader.readAsDataURL(file);
-                }
-              }}
+              onChange={handleImageUpload}
             />
             <p>Tire uma foto com seu celular</p>
+            {gender && <p>Gênero detectado: {gender}</p>}
           </div>
         )}
       </header>
